26299fbe4677ef9fb4dc27edd376f53380dff343
buchananc <candacebuchanan7@gmail.com>
2018-06-22 16:11:14 -0400

    added dependencies to server.js and controller.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,15 +1,13 @@
 // In controllers, create the burgers_controller.js file.
-// Inside the burgers_controller.js file,
-//     import the following:
-
-
-
-//     Express
+// Inside the burgers_controller.js file, import the following:
+// Express
 // burger.js
+// Create the router for the app, and export the router at the end of your file.
 
+var express = require('express');
+var router = express.Router();
 
-
-// Create the router
-// for the app, and
-// export the router at the end of your file.
+router.get('/', function (req,res) {
+    res.render('index');
+});
 
